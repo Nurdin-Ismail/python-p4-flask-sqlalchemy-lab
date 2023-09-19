da2cd9e2c1f93e802e5bf502b376ef00cf13b4d7
Nurdin Ismail <nurdin.ismail@student.moringaschool.com>
2023-09-19 15:38:34 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -19,15 +19,74 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal= Animal.query.filter(Animal.id == id).first()
+    
+    if not animal:
+        response_body = '<h1>404 animal not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    else:
+        response_body = f'''
+        <h1>Info about {animal.name}</h1>
+        <h3>Id: {animal.id}</h3>
+        <h3>Species: {animal.species}</h1>
+        <h3>Zookeeper: {animal.zookeeper.name}</h1>
+        <h3>Enclosure: {animal.enclosure.environment}</h3>
+        '''
+        response= make_response(response_body, 200)
+        return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    
+    
+            
+                
+    if not zookeeper:
+        response_body = '<h1>404 zookeeper not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    else:
+        
+        nemo = [i.name for i in zookeeper.animals]
+        response_body = f'''
+        <h1>Info about {zookeeper.name}</h1>
+        <h3>Birth Date: {zookeeper.birthday}</h3>
+        <h3> Animals:</h3>
+        <h3></h3>
+        ''' + 'Animal:' + ' <br> Animal: '.join([str(i) for i in nemo])
+        
+        
+        response= make_response(response_body, 200)
+        return response
+        
+        
+    
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    
+    
+    if not enclosure:
+        response_body = '<h1>404 enclosure not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    else:
+        
+        nemo = [i.name for i in enclosure.animals]
+        response_body = f'''
+        <h1>Id: {enclosure.id}</h1>
+        <h3>Enclosure: {enclosure.environment}</h3>
+        <h3> Animals:</h3>
+        <h3></h3>
+        ''' + 'Animal:' + ' <br> Animal: '.join([str(i) for i in nemo])
+        
+        
+        response= make_response(response_body, 200)
+        return response
+    
 
 
 if __name__ == '__main__':
